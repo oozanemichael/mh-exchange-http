d266a1edc57c86e442c5a511a67937e52b221af9
chuxianbo <chuxianbo@huobi.com>
2020-11-11 22:56:44 +0800

    exchange bibox coinSwap service 建立 完成marketdata-》getOrderBook

--- mh-bibox/src/main/java/org/market/hedge/bibox/coinswap/BiboxCoinSwapAuthenticated.java
+++ mh-bibox/src/main/java/org/market/hedge/bibox/coinswap/BiboxCoinSwapAuthenticated.java
@@ -5,6 +5,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+/**
+ * auth api
+ * */
 @Path("/")
 @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
 @Produces(MediaType.APPLICATION_JSON)
